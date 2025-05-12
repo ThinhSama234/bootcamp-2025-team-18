6a66c9e58b84a0bf27202926457ef26902f6be6b
Vuhuydiet <truongvu04072004@gmail.com>
2025-05-12 19:27:46 +0700

    fix processing-service

--- Backend/location-service/src/main.py
+++ Backend/location-service/src/main.py
@@ -18,6 +18,6 @@ if __name__ == "__main__":
 
   travel_db = MongoDB(TRAVELDB_URL, database="travel_db", collection="locations")
   travel_vector_db = MongoDB(TRAVELDB_URL, database="travel_db", collection="locations_vector")
-  handler = LocationServiceHandler(SuggestionService())
+  handler = LocationServiceHandler(SuggestionService(travel_db, travel_vector_db))
   asyncio.run(serve(handler))
 
